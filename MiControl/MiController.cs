8ca7bc601057befeb7a20a0781cf56bd72ce9206
Melvin van Kalsbeek <melvinvankalsbeek@ziggo.nl>
2015-10-22 15:51:03 +0200

    Removed unused 'usings'

--- MiControl/MiController.cs
+++ MiControl/MiController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Drawing;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Net.Sockets;
 
 namespace MiControl
