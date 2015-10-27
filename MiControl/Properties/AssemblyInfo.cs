b57591d0cd8068e6542a15b8ae5a8edde6925659
Melvin van Kalsbeek <melvinvankalsbeek@ziggo.nl>
2015-10-27 15:22:50 +0100

    Project properties edits

--- MiControl/Properties/AssemblyInfo.cs
+++ MiControl/Properties/AssemblyInfo.cs
@@ -1,27 +1,25 @@
-﻿using System.Reflection;
+using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiControl")]
-[assembly: AssemblyDescription("Control library for MiLight")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiControl")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("MiControl")]
+[assembly: AssemblyDescription ("Library for MiLight control")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("MiControl")]
+[assembly: AssemblyCopyright ("Copyright ©  2015")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f4bcb814-b639-487f-ab38-203642673190")]
-
+[assembly: Guid ("f4bcb814-b639-487f-ab38-203642673190")]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
