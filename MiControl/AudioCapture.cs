b6124aefe0aac5736e717cf9520dc1259bc42ec1
Melvin van Kalsbeek <melvinvankalsbeek@ziggo.nl>
2015-10-27 10:53:54 +0100

    Removed 'CSCore' usings
    Temporarily removed 'CSCore' integration from the project (kills Travis
    CI builds). Audio stuff will be added later on.

--- MiControl/AudioCapture.cs
+++ MiControl/AudioCapture.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
-using CSCore;
-using CSCore.SoundIn;
-using CSCore.Streams;
-using CSCore.Streams.SampleConverter;
-using CSCore.Streams.Effects;
-using CSCore.DSP;
 
 namespace MiControl
 {
@@ -14,22 +8,10 @@ namespace MiControl
     /// Class for capturing an audio stream and generating colors.
     /// </summary>
     public class AudioCapture
-    {
-    	WasapiLoopbackCapture capture;
-    	SoundInSource source;
-    	ISampleSource sampleSource;
-    	Equalizer eq;
-    	
+    {    	
     	public AudioCapture()
     	{
-    		capture = new WasapiLoopbackCapture();
-    		capture.Initialize();
-    		
-    		source = new SoundInSource(capture);
-    		
-    		sampleSource = WaveToSampleBase.CreateConverter(source);
-    		
-    		eq = new Equalizer(sampleSource);
+
     	}
     }
 }
