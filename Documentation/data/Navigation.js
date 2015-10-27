e3977e5aec9e320bdcc6c487e4053eeec3973f15
Melvin van Kalsbeek <melvinvankalsbeek@ziggo.nl>
2015-10-27 11:42:17 +0100

    Added sharpDox documentation
    Added library documentation

---
+++ Documentation/data/Navigation.js
@@ -0,0 +1,53 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+	{
+		id: 'MiControl',
+		text: 'MiControl',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/MiControl' },
+		children: [
+			
+ 
+
+		{
+			id: 'MiControl.AudioCapture',
+			text: 'AudioCapture',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/MiControl.AudioCapture' }
+        },	
+		{
+			id: 'MiControl.CaptureMethod',
+			text: 'CaptureMethod',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/MiControl.CaptureMethod' }
+        },	
+		{
+			id: 'MiControl.HSLColor',
+			text: 'HSLColor',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/MiControl.HSLColor' }
+        },	
+		{
+			id: 'MiControl.MiController',
+			text: 'MiController',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/MiControl.MiController' }
+        },	
+		{
+			id: 'MiControl.ScreenColor',
+			text: 'ScreenColor',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/MiControl.ScreenColor' }
+        },	
+
+
+		]
+	},
+
+
+	];
---
+++ Documentation/data/Project.js
@@ -0,0 +1,11 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.projectData = {
+  name: 'MiControl',
+   homepage: 'https://github.com/Milfje/MiControl',      author: 'Melvin van Kalsbeek',         hasLogo: false,
+  targetFxs: [
+			"Unknown",
+	  ]
+}
